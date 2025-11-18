d65082be224b76c821bd7bd08f462fde4a1288cd
Luiz Francisco <luizfk.marinho@gmail.com>
2025-11-18 11:13:12 -0300

    Configurando model de cliente, database connection e views

---
+++ src/main/java/com/luizfrancisco/estacionamento/Estacionamento.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.luizfrancisco.estacionamento;
+
+/**
+ *
+ * @author User
+ */
+public class Estacionamento {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+       
+    }
+}
---
+++ src/main/java/com/luizfrancisco/estacionamento/model/Cliente.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.luizfrancisco.estacionamento.model;
+
+/**
+ *
+ * @author User
+ */
+public class Cliente {
+    private int id;
+    private String nome;
+    private String cpf;
+
+    public Cliente(int id, String nome, String cpf) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    
+    
+}
