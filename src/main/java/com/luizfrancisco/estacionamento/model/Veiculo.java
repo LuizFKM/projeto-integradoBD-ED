19118dce74e83af120bb302c1d05a64ecea6d303
Luiz Francisco <luizfk.marinho@gmail.com>
2025-11-22 20:32:16 -0300

    criando model de veiculo

---
+++ src/main/java/com/luizfrancisco/estacionamento/model/Veiculo.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.luizfrancisco.estacionamento.model;
+
+/**
+ *
+ * @author User
+ */
+public class Veiculo {
+    private int id;
+    private int idCliente;
+    private String modelo;
+    private String cor;
+    private String placa;
+
+    public Veiculo(int id, int idCliente, String modelo, String cor, String placa) {
+        this.id = id;
+        this.idCliente = idCliente;
+        this.modelo = modelo;
+        this.cor = cor;
+        this.placa = placa;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    @Override
+    public String toString() {
+        return "Veiculo{" + "id=" + id + ", idCliente=" + idCliente + ", modelo=" + modelo + ", cor=" + cor + ", placa=" + placa + '}';
+    }
+    
+    
+}
